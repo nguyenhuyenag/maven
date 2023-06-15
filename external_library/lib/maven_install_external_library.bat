8c9296a283060e75bf977decf39b9c5758510c3e
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-15 10:29:30 +0700

    #

---
+++ external_library/lib/maven_install_external_library.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set "file=data.txt"
+set LIB_DIR=D:\Dev\Projects\Github\maven\external_library\lib
+
+for /f "usebackq tokens=1-4 delims=," %%a in ("%file%") do (
+	echo File install: filename=%%a, groupId=%%b, artifactId=%%c, version=%%d
+	echo.
+	call mvn install:install-file -Dfile=%LIB_DIR%\%%a -DgroupId=%%b -DartifactId=%%c -Dversion=%%d -Dpackaging=jar -Durl=file:./lib/ -DrepositoryId=lib -DupdateReleaseInfo=true
+	echo.
+)
+
+timeout /t 5 /nobreak
+exit
