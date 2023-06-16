02e4fa62d55570753ea2685c78cdecdcd5c4634a
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-16 08:26:56 +0700

    #

--- external_library/lib/autorun/maven_install_external_library.bat
+++ external_library/lib/autorun/maven_install_external_library.bat
@@ -1,10 +1,12 @@
 @echo off
 
 set "file=data.txt"
-set LIB_DIR=D:\Dev\Projects\Github\maven\external_library\lib
+for %%I in ("%CD%") do set "LIB_DIR=%%~dpI"
+::set LIB_DIR=D:\Dev\Projects\Github\maven\external_library\lib
 
 for /f "usebackq tokens=1-4 delims=," %%a in ("%file%") do (
-	echo [FILE INFO]: filename=%%a, groupId=%%b, artifactId=%%c, version=%%d
+	echo.
+	echo [INFO]: filename=%%a, groupId=%%b, artifactId=%%c, version=%%d
 	echo.
 	call mvn install:install-file ^
 		-Dfile=%LIB_DIR%\%%a ^
