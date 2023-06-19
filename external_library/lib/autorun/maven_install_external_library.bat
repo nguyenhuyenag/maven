f9cb3c6f83d549194b459edffed6a6c67e5df887
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-19 09:00:18 +0700

    #

--- external_library/lib/autorun/maven_install_external_library.bat
+++ external_library/lib/autorun/maven_install_external_library.bat
@@ -1,13 +1,21 @@
 @echo off
 
-set "file=data.txt"
-for %%I in ("%CD%") do set "LIB_DIR=%%~dpI"
-::set LIB_DIR=D:\Dev\Projects\Github\maven\external_library\lib
+set "INPUT_FILE=input.txt"
+set "OUTPUT_FILE=%CD%\output.txt"
 
-for /f "usebackq tokens=1-4 delims=," %%a in ("%file%") do (
+if exist %OUTPUT_FILE% (
+    del %OUTPUT_FILE%
+	echo [INFO]: File deleted successfully
+)
+
+for %%I in ("%CD%") do set "LIB_DIR=%%~dpI"
+
+for /f "usebackq tokens=1-4 delims=," %%a in ("%INPUT_FILE%") do (
 	echo.
+	REM File info
 	echo [INFO]: filename=%%a, groupId=%%b, artifactId=%%c, version=%%d
 	echo.
+	REM Install jar
 	call mvn install:install-file ^
 		-Dfile=%LIB_DIR%\%%a ^
 		-DgroupId=%%b ^
@@ -18,6 +26,15 @@ for /f "usebackq tokens=1-4 delims=," %%a in ("%file%") do (
 		-DrepositoryId=lib ^
 		-DupdateReleaseInfo=true
 	echo.
+	REM Create dependency
+	(
+        echo ^<dependency^>
+        echo     ^<groupId^>%%b^</groupId^>
+        echo     ^<artifactId^>%%c^</artifactId^>
+        echo     ^<version^>%%d^</version^>
+        echo ^</dependency^>
+    ) >> %OUTPUT_FILE%
+	echo Content appended successfully.
 )
 
 timeout /t 5 /nobreak
