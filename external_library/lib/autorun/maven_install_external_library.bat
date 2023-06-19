16d49c51b48627b4d2ede41c71774d4d7514e24b
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-19 09:21:51 +0700

    #

--- external_library/lib/autorun/maven_install_external_library.bat
+++ external_library/lib/autorun/maven_install_external_library.bat
@@ -5,37 +5,40 @@ set "OUTPUT_FILE=%CD%\output.txt"
 
 if exist %OUTPUT_FILE% (
     del %OUTPUT_FILE%
-	echo [INFO]: File deleted successfully
+	echo [INFO] Delete 'output.txt'
 )
 
+REM Get jar library
 for %%I in ("%CD%") do set "LIB_DIR=%%~dpI"
 
 for /f "usebackq tokens=1-4 delims=," %%a in ("%INPUT_FILE%") do (
 	echo.
-	REM File info
-	echo [INFO]: filename=%%a, groupId=%%b, artifactId=%%c, version=%%d
-	echo.
 	REM Install jar
-	call mvn install:install-file ^
-		-Dfile=%LIB_DIR%\%%a ^
-		-DgroupId=%%b ^
-		-DartifactId=%%c ^
-		-Dversion=%%d ^
-		-Dpackaging=jar ^
-		-Durl=file:./lib/ ^
-		-DrepositoryId=lib ^
-		-DupdateReleaseInfo=true
-	echo.
-	REM Create dependency
-	(
-        echo ^<dependency^>
-        echo     ^<groupId^>%%b^</groupId^>
-        echo     ^<artifactId^>%%c^</artifactId^>
-        echo     ^<version^>%%d^</version^>
-        echo ^</dependency^>
-    ) >> %OUTPUT_FILE%
-	echo Content appended successfully.
+	if not exist %LIB_DIR%\%%a (
+		echo [INFO] File '%%a' doesn't exist!
+	) else (
+		REM File infomation
+		echo [INFO] Install jar for 'filename=%%a, groupId=%%b, artifactId=%%c, version=%%d'
+		call mvn install:install-file ^
+			-Dfile=%LIB_DIR%\%%a ^
+			-DgroupId=%%b ^
+			-DartifactId=%%c ^
+			-Dversion=%%d ^
+			-Dpackaging=jar ^
+			-Durl=file:./lib/ ^
+			-DrepositoryId=lib ^
+			-DupdateReleaseInfo=true
+		REM Create dependency
+		echo [INFO] Create dependency
+		(
+			echo ^<dependency^>
+			echo     ^<groupId^>%%b^</groupId^>
+			echo     ^<artifactId^>%%c^</artifactId^>
+			echo     ^<version^>%%d^</version^>
+			echo ^</dependency^>
+		) >> %OUTPUT_FILE%
+	)
 )
 
-timeout /t 5 /nobreak
+timeout /t 30 /nobreak
 exit
